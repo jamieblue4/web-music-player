ec2ac149ce2d31bbae1840ab744db99e521f8658
Jamie Gist <jamie.gist.7@gmail.com>
2023-01-06 23:24:02 -0500

    More setup.

--- spotify-clone-revision/src/App.js
+++ spotify-clone-revision/src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{}, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -21,13 +24,13 @@ function App() {
       spotify.setAccessToken(_token);
 
       spotify.getMe().then(user => {
-      })
+      });
     }
   }, []);
 
   return (
     <div className="app">
-      {token ? <h1>I'm in :P </h1> : (<Login />)}
+      {token ? <Player /> : (<Login />)}
     </div>
   );
 }
