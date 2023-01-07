a1fa6143dd247bb0acc5322c6be8136db062985e
Jamie Gist <jamie.gist.7@gmail.com>
2023-01-07 00:31:14 -0500

    Basic sketch of the app.

---
+++ spotify-clone-revision/src/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <div class="sidebar">
+        <img className="sidebar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""></img>
+    </div>
+  )
+}
+
+export default Sidebar;
